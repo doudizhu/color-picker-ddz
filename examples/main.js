57a9ce5d4f094ffaf39d19dfa965bd5334f4033e
lipeng <lipeng@daokoudai.com>
2021-09-11 21:44:38 +0800

    4.编写示例

--- examples/main.js
+++ examples/main.js
@@ -3,6 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import hljs from 'highlight.js'
+import 'highlight.js/styles/railscasts.css'
+Vue.directive('hljs', el => {
+  let blocks = el.querySelectorAll('pre')
+  Array.prototype.forEach.call(blocks, hljs.highlightBlock)
+})
+
+// 导入组件库
+import ColorPicker from '../packages/index'
+// 注册组件库
+Vue.use(ColorPicker)
+
 Vue.config.productionTip = false
 
 new Vue({
